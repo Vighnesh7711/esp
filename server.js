98302621158f00d7d91ec10f0da0dcd76b3e0315
Vighnesh Gawande <vighneshgawande@gmail.com>
2026-07-11 20:17:53 +0530

    Update MongoDB URI for connection

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname)));
 
 // 🔗 MongoDB connection
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/smart_classroom";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://Vighnesh:Blade%402004@cluster0.uqi1sas.mongodb.net/?appName=Cluster0";
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
